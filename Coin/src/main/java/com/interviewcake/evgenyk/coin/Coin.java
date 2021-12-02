ac343af1c05c38a44956087c780d116a45f63040
lopotun <lopotun@gmail.com>
2021-12-02 23:09:43 +0200

    Fixes in "Coin" module.

--- Coin/src/main/java/com/interviewcake/evgenyk/coin/Coin.java
+++ Coin/src/main/java/com/interviewcake/evgenyk/coin/Coin.java
@@ -5,44 +5,37 @@ import com.interviewcake.evgenyk.Utils;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
-import java.util.stream.Collectors;
 
 /**
  * https://www.interviewcake.com/question/java/coin
  */
 public class Coin {
-    List<List<Integer>> stack = new ArrayList<>();
-    /*
-    17 (15, 10, 5, 3, 2, 1):
-    15 + 2;     15 + 1 + 1
-    10 + 5 + 2;     10 + 5 + 1 + 1;     10 + 3 + 3 +1;      10 + 3 + 2 + 2;     10 + 3 + 2 + 1 + 1;     10 + 3 + 1 + 1 + 1 + 1
-    5 + 5 + 5 + 2;      5 + 5 + 5 + 1 + 1;      5 + 5 + 3 + 3 + 1;      5 + 5 + 3 + 2 + 2;       5 + 5 + 3 + 2 + 1 +1
-     */
-    public void parse(int amount, List<Integer> denominations) {
-        LinkedList<Integer> res = new LinkedList<>();
-        prs(amount, denominations, res);
+    public List<LinkedList<Integer>> parse(int amount, List<Integer> denominations) {
+        LinkedList<Integer> combination = new LinkedList<>();
+        List<LinkedList<Integer>> res = new ArrayList<>();
+        prs(amount, denominations, combination, res);
+        return res;
     }
-    public void prs(int amount, List<Integer> denominations, LinkedList<Integer> res) {
+
+    private void prs(int amount, List<Integer> denominations, LinkedList<Integer> combination, List<LinkedList<Integer>> res) {
         if(denominations.isEmpty()) {
-            if(!res.isEmpty())
-                res.removeLast();
+            if(!combination.isEmpty())
+                combination.removeLast();
             return;
         }
         int coin = Utils.head(denominations);
         int delta = amount - coin;
-        if(delta < 0) {
-            prs(amount, Utils.tail(denominations), res);
-        }
+        if(delta < 0)
+            prs(amount, Utils.tail(denominations), combination, res);
         else {
-            res.addLast(coin);
+            combination.addLast(coin);
             if(delta > 0)
-                prs(delta, denominations, res);
+                prs(delta, denominations, combination, res);
             else {
-                stack.add(new ArrayList<>(res));
-                res.removeLast();
+                res.add(new LinkedList<>(combination));
+                combination.removeLast();
             }
-            prs(amount, Utils.tail(denominations), res);
+            prs(amount, Utils.tail(denominations), combination, res);
         }
     }
 }
--- Coin/src/test/java/com/interviewcake/evgenyk/coin/CoinTest.java
+++ Coin/src/test/java/com/interviewcake/evgenyk/coin/CoinTest.java
@@ -2,6 +2,8 @@ package com.interviewcake.evgenyk.coin;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,25 +13,26 @@ class CoinTest {
     @Test
     void prs17() {
         Coin x = new Coin();
-        x.parse(17, List.of(15, 10, 5, 3, 2, 1));
-        System.out.println(x.stack);
+        List<LinkedList<Integer>> res = x.parse(17, List.of(15, 10, 5, 3, 2, 1));
+        //noinspection RedundantTypeArguments (explicit type arguments speedup compilation and analysis time)
+        assertEquals(res, List.<List<Integer>>of(List.of(15, 2), List.of(15, 1, 1), List.of(10, 5, 2), List.of(10, 5, 1, 1), List.of(10, 3, 3, 1), List.of(10, 3, 2, 2), List.of(10, 3, 2, 1, 1), List.of(10, 3, 1, 1, 1, 1), List.of(10, 2, 2, 2, 1), List.of(10, 2, 2, 1, 1, 1), List.of(10, 2, 1, 1, 1, 1, 1), List.of(10, 1, 1, 1, 1, 1, 1, 1), List.of(5, 5, 5, 2), List.of(5, 5, 5, 1, 1), List.of(5, 5, 3, 3, 1), List.of(5, 5, 3, 2, 2), List.of(5, 5, 3, 2, 1, 1), List.of(5, 5, 3, 1, 1, 1, 1), List.of(5, 5, 2, 2, 2, 1), List.of(5, 5, 2, 2, 1, 1, 1), List.of(5, 5, 2, 1, 1, 1, 1, 1), List.of(5, 5, 1, 1, 1, 1, 1, 1, 1), List.of(5, 3, 3, 3, 3), List.of(5, 3, 3, 3, 2, 1), List.of(5, 3, 3, 3, 1, 1, 1), List.of(5, 3, 3, 2, 2, 2), List.of(5, 3, 3, 2, 2, 1, 1), List.of(5, 3, 3, 2, 1, 1, 1, 1), List.of(5, 3, 3, 1, 1, 1, 1, 1, 1), List.of(5, 3, 2, 2, 2, 2, 1), List.of(5, 3, 2, 2, 2, 1, 1, 1), List.of(5, 3, 2, 2, 1, 1, 1, 1, 1), List.of(5, 3, 2, 1, 1, 1, 1, 1, 1, 1), List.of(5, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(5, 2, 2, 2, 2, 2, 2), List.of(5, 2, 2, 2, 2, 2, 1, 1), List.of(5, 2, 2, 2, 2, 1, 1, 1, 1), List.of(5, 2, 2, 2, 1, 1, 1, 1, 1, 1), List.of(5, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1), List.of(5, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(3, 3, 3, 3, 3, 2), List.of(3, 3, 3, 3, 3, 1, 1), List.of(3, 3, 3, 3, 2, 2, 1), List.of(3, 3, 3, 3, 2, 1, 1, 1), List.of(3, 3, 3, 3, 1, 1, 1, 1, 1), List.of(3, 3, 3, 2, 2, 2, 2), List.of(3, 3, 3, 2, 2, 2, 1, 1), List.of(3, 3, 3, 2, 2, 1, 1, 1, 1), List.of(3, 3, 3, 2, 1, 1, 1, 1, 1, 1), List.of(3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1), List.of(3, 3, 2, 2, 2, 2, 2, 1), List.of(3, 3, 2, 2, 2, 2, 1, 1, 1), List.of(3, 3, 2, 2, 2, 1, 1, 1, 1, 1), List.of(3, 3, 2, 2, 1, 1, 1, 1, 1, 1, 1), List.of(3, 3, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(3, 2, 2, 2, 2, 2, 2, 2), List.of(3, 2, 2, 2, 2, 2, 2, 1, 1), List.of(3, 2, 2, 2, 2, 2, 1, 1, 1, 1), List.of(3, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1), List.of(3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1), List.of(3, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(3, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(2, 2, 2, 2, 2, 2, 2, 2, 1), List.of(2, 2, 2, 2, 2, 2, 2, 1, 1, 1), List.of(2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1), List.of(2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1), List.of(2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), List.of(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)));
     }
     @Test
     void prs4() {
         Coin x = new Coin();
-        x.parse(4, List.of(15, 10, 5, 3, 2, 1));
-        System.out.println(x.stack);
+        List<LinkedList<Integer>> res = x.parse(4, List.of(15, 10, 5, 3, 2, 1));
+        assertEquals(res, List.of(List.of(3,1), List.of(2,2), List.of(2,1,1), List.of(1,1,1,1)));
     }
     @Test
     void prs0() {
         Coin x = new Coin();
-        x.parse(0, List.of(15, 10, 5, 3, 2, 1));
-        System.out.println(x.stack);
+        List<LinkedList<Integer>> res = x.parse(0, List.of(15, 10, 5, 3, 2, 1));
+        assertEquals(res, Collections.emptyList());
     }
     @Test
     void prsNone() {
         Coin x = new Coin();
-        x.parse(17, List.of(15, 10, 5));
-        System.out.println(x.stack);
+        List<LinkedList<Integer>> res = x.parse(17, List.of(15, 10, 5));
+        assertEquals(res, Collections.emptyList());
     }
 }
