28a399cc18700c2692636e1fc95299a7dc14a7e9
Evgeny Kurtser <EvgenyK@traiana.com>
2021-12-12 15:27:21 +0200

    Pair<A, B> class optimization.

--- src/main/java/com/interviewcake/evgenyk/Pair.java
+++ src/main/java/com/interviewcake/evgenyk/Pair.java
@@ -7,27 +7,10 @@ import lombok.*;
  * <a href=mailto:lopotun@gmail.com>lopotun@gmail.com</a>
  */
 @Data
+@AllArgsConstructor
 public class Pair<A, B> {
-    private A a;
-    private B b;
-
-    public Pair(A a, B b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    public Pair(Pair<A, B> other) {
-        this.a = other.a;
-        this.b = other.b;
-    }
-
-    public Pair<A, B> withA(A a) {
-        return new Pair<>(a, this.b);
-    }
-
-    public Pair<A, B> withB(B b) {
-        return new Pair<>(this.a, b);
-    }
+    @With private A a;
+    @With private B b;
 
     public static <A, B> Pair<A, B> of(A a, B b) {
         return new Pair<>(a, b);
